dbfef81b4b52825c7e48dd492a4a6004a42c7cb5
Stanislav Liashkov <stanislavliashkov@gmail.com>
2025-02-12 23:07:50 +0700

    added comments

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <exception>
 
+
+// ALGORITHM EXPLANATION
+// If charcter is number, we push this number onto stack.
+// If character is an operator, we pop two numbers from the top of the stack and apply operator. 
+// Push result back to the stack.
+// If RNP experssion was valid, at the end of the processing input, we have stack with single number which is RESULT
+//
+
+
 int main(int argc, char **argv) {
     try {
         if (argc == 2) {
